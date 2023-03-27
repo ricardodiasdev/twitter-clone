497a6f850b47c623cf18b2eece09779ca5788160
Ricardo Dias <ricardodias.dev@gmail.com>
2023-03-27 19:02:24 -0300

    Corrigindo Input e criando o componente LoginModal

--- components/Input.tsx
+++ components/Input.tsx
@@ -6,6 +6,7 @@ interface InputProps {
   type?: string;
   disabled: boolean;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  label?:string;
 }
 const Input: React.FC<InputProps> = ({
   placeholder,
@@ -13,32 +14,37 @@ const Input: React.FC<InputProps> = ({
   type,
   disabled,
   onChange,
+  label
 }) => {
   return (
-    <input
-    disabled={disabled}
-    onChange={onChange}
-    value={value}
-    placeholder={placeholder}
-    type={type}
-    className="
-        w-full
-        p-4
-        text-lg
-        bg-black
-        border-2
-        border-neutral-800
-        rounded-md
-        outline-none
-        text-white
-        focus:border-sky-500
-        focus:border-2
-        transition
-        disabled: opacity-70
-        disabled:cursor-not-allowed
-    "
-    >Enter</input>
-);
-};
-
+    <div className="w-full">
+      {label && <p className="text-xl text-white font-semibold mb-2">{label}</p>}
+      <input
+        disabled={disabled}
+        onChange={onChange}
+        value={value}
+        placeholder={placeholder}
+        type={type}
+        className="
+          w-full
+          p-4 
+          text-lg 
+          bg-black 
+          border-2
+          border-neutral-800 
+          rounded-md
+          outline-none
+          text-white
+          focus:border-sky-500
+          focus:border-2
+          transition
+          disabled:bg-neutral-900
+          disabled:opacity-70
+          disabled:cursor-not-allowed
+        "
+      />
+    </div>
+   );
+}
+ 
 export default Input;
