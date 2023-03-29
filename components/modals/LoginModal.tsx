b089d72b36a089c2c2bae58d4293e1eeac5b87ee
Ricardo Dias <ricardodias.dev@gmail.com>
2023-03-28 21:44:46 -0300

    Habilitando o login

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,5 +1,6 @@
 import useLoginModal from "@/hooks/useLoginModal";
 import useRegisterModal from "@/hooks/useRegisterModal";
+import { signIn } from "next-auth/react";
 import { useCallback, useState } from "react";
 import Input from "../Input";
 import Modal from "../Modal";
@@ -22,14 +23,14 @@ const LoginModal = () => {
   const onSubmit = useCallback(async () => {
     try {
       setIsLoading(true);
-      //todo add log IN
+      await signIn("credentials", { email, password });
       loginModal.onClose();
     } catch (error) {
       console.log(error);
     } finally {
       setIsLoading(false);
     }
-  }, [loginModal]);
+  }, [loginModal, email, password]);
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
@@ -42,6 +43,7 @@ const LoginModal = () => {
       <Input
         placeholder="Password"
         onChange={(e) => setPassword(e.target.value)}
+        type='password'
         value={password}
         disabled={isLoading}
       />
